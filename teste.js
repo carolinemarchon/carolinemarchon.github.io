83892ddd608a7daa9464b1c3e60ef7388418f5a4
carolinemarchon <73369642+carolinemarchon@users.noreply.github.com>
2021-01-03 14:06:07 -0300

    Add files via upload

---
+++ teste.js
@@ -0,0 +1,68 @@
+﻿var imagem1 = document.querySelector('#menu1');
+var imagem2 = document.querySelector('#menu2');
+var imagem3 = document.querySelector('#menu3');
+var imagem4 = document.querySelector('#menu4');
+var imagem5 = document.querySelector('#menu5');
+var imagem6 = document.querySelector('#menu6');
+
+imagem1.addEventListener('mouseenter', function(event){
+        
+imagem1.src ='saudefisica.png';
+});
+  
+imagem1.addEventListener('mouseout', function(event){
+        
+imagem1.src ='imgsaudefisica.jpg';
+});
+
+imagem2.addEventListener('mouseenter', function(event){
+        
+imagem2.src ='saudemulher.png';
+});
+  
+imagem2.addEventListener('mouseout', function(event){
+        
+imagem2.src ='imgsaudemulher.jpg';
+});
+
+imagem3.addEventListener('mouseenter', function(event){
+        
+imagem3.src ='saudeinfantil.png';
+});
+  
+imagem3.addEventListener('mouseout', function(event){
+        
+imagem3.src ='imgsaudeinfantil.jpg';
+});
+
+imagem4.addEventListener('mouseenter', function(event){
+        
+imagem4.src ='saudemental.png';
+});
+  
+imagem4.addEventListener('mouseout', function(event){
+        
+imagem4.src ='imgsaudemental.png';
+});
+
+imagem5.addEventListener('mouseenter', function(event){
+        
+imagem5.src ='idosos.png';
+});
+  
+imagem5.addEventListener('mouseout', function(event){
+        
+imagem5.src ='imgidosos.jpg';
+});
+
+imagem6.addEventListener('mouseenter', function(event){
+        
+imagem6.src ='naosei.png';
+});
+  
+imagem6.addEventListener('mouseout', function(event){
+        
+imagem6.src ='naosei.jpg';
+});
+
+
